4dd09d454d2911b9581e3efbb9b19927fd50f7f3
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-06-15 22:15:20 +0200

    feat: add cli-args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  const parsedArgs = {};
+
+  for (let i = 2; i < args.length; i += 2) {
+    const propName = args[i].substring(2);
+    const value = args[i + 1];
+
+    parsedArgs[propName] = value;
+  }
+
+  const formattedArgs = Object.entries(parsedArgs).map(
+    ([propName, value]) => `${propName} is ${value}`
+  );
+
+  console.log(formattedArgs.join(", "));
 };
 
 parseArgs();
