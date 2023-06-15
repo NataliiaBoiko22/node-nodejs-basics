2043b04d97b1f2b31bff79b98587da79c11a55af
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-06-15 19:43:58 +0200

    feat: add read-fs

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = "./files";
+
+  try {
+    await fs.access(folderPath);
+    const files = await fs.readdir(folderPath);
+    console.log("File List:");
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "./files/fileToRead.txt";
+
+  try {
+    const fileContent = await fs.readFile(filePath, "utf8");
+    console.log(fileContent);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      throw err;
+    }
+  }
 };
 
 await read();
