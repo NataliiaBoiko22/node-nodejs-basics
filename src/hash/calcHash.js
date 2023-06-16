1d53fa795d2b0ae3ddb8fff9f781bd161910faf0
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-06-16 09:33:57 +0200

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,35 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = "./files/fileToCalculateHashFor.txt";
+  const hash = crypto.createHash("sha256");
+  const input = fs.createReadStream(filePath);
+  return new Promise((resolve, reject) => {
+    input.on("readable", () => {
+      const data = input.read();
+      if (data) {
+        hash.update(data);
+      } else {
+        const fileHash = hash.digest("hex");
+        resolve(fileHash);
+      }
+    });
+
+    input.on("error", (error) => {
+      reject(error);
+    });
+  });
 };
+
+const fileToCalculateHashFor = "fileToCalculateHashFor.txt";
+
+calculateHash(fileToCalculateHashFor)
+  .then((fileHash) => {
+    console.log("SHA256 Hash:", fileHash);
+  })
+  .catch((error) => {
+    console.error("Error calculating hash:", error);
+  });
 
 await calculateHash();
